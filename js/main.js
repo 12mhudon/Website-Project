c4d44b10b36c214a42befa9db3b2a3f3e49b2baa
12mhudon <128413833+12mhudon@users.noreply.github.com>
2023-04-23 13:00:01 -0400

    undo portfolio not load on start. add refresh on page load

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+window.onload = function() {
+    if(!window.location.hash) {
+        window.location = window.location + '#loaded';
+        window.location.reload();
+    }
+}
+
 (function ($) {
     "use strict";
 
